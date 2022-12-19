ddc654d8b20b4ccdd29f1b9e2946d99db030474f
RADHIARH <radhia.rahmani@sesame.com.tn>
2022-12-19 15:40:50 +0100

    send changes

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
 var mysql = require("mysql2");
 var dbConn = mysql.createConnection({
-  host: "127.0.0.1",
+  host: "localhost",
   user: "root",
   password: "reactjs!nodejs$ingenieurinformatique",
   database: "mydatabase",
   multipleStatements: true,
-  port: "8080",
 });
 module.exports = dbConn;
